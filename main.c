1382fdd201d09f3318031d52d3cd6980dc69d534
WindArrow <wind.dong.dream@gmail.com>
2016-06-29 11:41:58 +0900

    게임규칙, 게임 종료

--- main.c
+++ main.c
@@ -1,5 +1,53 @@
 #include <stdio.h>
 
+int choiceMenu();
+void showRule();
+
 int main() {
+    while (1) {
+        switch (choiceMenu()) {
+            case 1:
+                // 게임 시작
+                break;
+
+            case 2:
+                // 게임 규칙
+                showRule();
+                break;
+
+            case 3:
+                // 게임 종료
+                return 0;
+        }
+    }
     return 0;
+}
+
+
+/*
+ * TODO : 메뉴 보여주기
+ * */
+int choiceMenu() {
+    int menu = 0;
+    printf("1. 게임시작\n");
+    printf("2. 게임규칙\n");
+    printf("3. 게임종료\n");
+    scanf("%d", &menu);
+    return menu;
+}
+
+
+/*
+ * TODO : 규칙 보여주기
+ */
+void showRule() {
+    printf("게임 규칙\n\n");
+    printf("-처음에 판 가운데에 사각형으로 엇갈리게 배치된 돌 4개를 놓고 시작한다.\n");
+    printf("-돌은 반드시 상대방 돌을 양쪽에서 포위하여 뒤집을 수 있는 곳에 놓아야 한다.\n");
+    printf("-돌을 뒤집을 곳이 없는 경우에는 차례가 자동적으로 상대방에게 넘어가게 된다.\n");
+    printf("-아래와 같은 조건에 의해 양쪽 모두 더 이상 돌을 놓을 수 없게 되면 게임이 끝나게 된다.\n");
+    printf("\t64개의 돌 모두가 판에 가득 찬 경우\n");
+    printf("\t어느 한 쪽이 돌을 모두 뒤집은 경우\n");
+    printf("\t한 차례에 양 쪽 모두 서로 차례를 넘겨야 하는 경우\n");
+    printf("-게임이 끝났을 때 돌이 많이 있는 플레이어가 승자가 된다. 만일 돌의 개수가 같을 경우는 무승부가 된다.\n\n\n");
 }
